b0a7b91d226ae763a1c30f4f87de66adc8e4fb18
Ivan Micai <ivanmicai@gmail.com>
2016-02-25 02:15:32 -0300

    update created_at order

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,9 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
   	if(params['topic'])
-  		@posts = Post.where("topic_category_#{params['topic'].split(",").length+1}": params['topic']).order(created_at: :asc).limit(100)
+  		@posts = Post.where("topic_category_#{params['topic'].split(",").length+1}": params['topic']).order(created_at: :desc).limit(100)
   	else
-  		@posts = Post.all.limit(100)
+  		@posts = Post.all.order(created_at: :desc).limit(100)
   	end
   end
   
