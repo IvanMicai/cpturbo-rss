68501c45b814b25c963e05e46ce7c9f212212ab9
Ivan Micai <ivanmicai@gmail.com>
2016-02-25 12:25:11 -0300

    Posts: topic filter fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
   	if(params['topic'])
-  		@posts = Post.where("topic_category_#{params['topic'].split(",").length+1}": params['topic']).order(created_at: :desc).limit(100)
+  		@posts = Post.where("topic_category_#{params['topic'].split(".").length}": params['topic']).order(created_at: :desc).limit(100)
   	else
   		@posts = Post.all.order(created_at: :desc).limit(100)
   	end
